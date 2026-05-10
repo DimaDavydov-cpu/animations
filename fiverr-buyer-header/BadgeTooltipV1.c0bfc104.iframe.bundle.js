2d43dba15f8f78136b8797d9aa1061bced416976
Dima Davydov <dima.davydov@Dimas-MacBook-Pro.local>
2026-05-10 15:57:05 +0300

    Add fiverr-buyer-header storybook

---
+++ fiverr-buyer-header/BadgeTooltipV1.c0bfc104.iframe.bundle.js
@@ -0,0 +1,18 @@
+"use strict";
+(self["webpackChunk_fiverr_private_penta_playground"] = self["webpackChunk_fiverr_private_penta_playground"] || []).push([[46],{
+
+/***/ "../badges/dist/esm/src/components/helpers/BadgeTooltip/index.js":
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _fiverr_private_tooltip__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("../tooltip/dist/esm/src/components/Tooltip/index.js");
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_fiverr_private_tooltip__WEBPACK_IMPORTED_MODULE_0__/* ["default"] */ .A);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=BadgeTooltipV1.c0bfc104.iframe.bundle.js.map
